13b35386a9459a6c2b559b24b5c0bb6cba86aa6d
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-10-27 16:41:45 +0100

    create project structure to integrate key tools

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+        colors: {
+            primary: {
+                DEFAULT: '#34967C',
+                100: '#F0FFFB'
+            }
+        }
+    },
+  },
+  plugins: [],
+}
